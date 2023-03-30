0d93fae1a2f37f140b0675fd5c02c52f26c5796c
Uichae <ghddmlco@gmail.com>
2023-03-30 18:41:23 +0900

    dynamic 수업

--- 09_form_dynamic/app.js
+++ 09_form_dynamic/app.js
@@ -1,4 +1,4 @@
-const { query } = require("express");
+// const { query } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 8000;
@@ -7,6 +7,22 @@ app.set("view engine", "ejs"); // view engine 등록
 app.use("/views", express.static(__dirname + "/views")); // ejs를 담을 view 폴더 경로 설정
 app.use(express.urlencoded({ extended: true })); // post 요청으로 들어오는 모든 데이터를 파싱
 app.use(express.json()); // json 형태로 데이터를 주고 받음
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+// 1-1 ajax get 요청
+app.get("/ajax", function (req, res) {
+  console.log(req.query);
+  res.send(req.query);
+});
+
+// 1-2 /ajax post 요청
+app.post("/ajax", function (req, res) {
+  console.log(req.body);
+  res.send(req.body);
+});
 
 app.listen(PORT, function () {
   console.log("웹 서버 실행");
