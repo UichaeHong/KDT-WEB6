0dd6547dea630f0f25a27cceb6f7406ab018f532
Uichae <ghddmlco@gmail.com>
2023-03-30 17:39:52 +0900

    폴더 생성

---
+++ 09_form_dynamic/app.js
@@ -0,0 +1,14 @@
+const { query } = require("express");
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.set("view engine", "ejs"); // view engine 등록
+app.use("/views", express.static(__dirname + "/views")); // ejs를 담을 view 폴더 경로 설정
+app.use(express.urlencoded({ extended: true })); // post 요청으로 들어오는 모든 데이터를 파싱
+app.use(express.json()); // json 형태로 데이터를 주고 받음
+
+app.listen(PORT, function () {
+  console.log("웹 서버 실행");
+  console.log(`http://localhost:${PORT}`);
+});
