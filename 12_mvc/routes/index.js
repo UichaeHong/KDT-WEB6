296d1241f2b485220fe25c08635509101a871799
Uichae <ghddmlco@gmail.com>
2023-04-04 18:10:03 +0900

    mvc 수업

---
+++ 12_mvc/routes/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const controller = require("../controller/Cmain");
+const router = express.Router();
+// controller = {
+//   main: () => {}, // 함수
+//   comments: ()=> {} // 함수
+// };
+
+// const app = express();
+// app.get
+
+// router.get("/", (req, res) => {
+//   res.render("index");
+// });
+
+router.get("/", controller.main);
+
+router.get("/comments", controller.comments);
+router.get("/comment/:id", controller.comment);
+
+// router.get("/comments", (req, res) => {
+//   console.log(comments); // 댓글 목록이 [ {}, {}, {}, {} ] 형태로 출력
+//   res.render("comments", { commentInfos: comments });
+// });
+
+// 작성하지 않으면 모듈을 찾을 수 없다고 뜬다
+module.exports = router;
