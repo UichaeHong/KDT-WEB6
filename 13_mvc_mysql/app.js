6fc8fb40ba08b703bf524b7b80c59becf76ec48f
Uichae <ghddmlco@gmail.com>
2023-04-05 18:20:21 +0900

    수정

--- 13_mvc_mysql/app.js
+++ 13_mvc_mysql/app.js
@@ -8,6 +8,13 @@ app.use("/static", express.static(__dirname + "/static"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// 라우터 분리
+const indexRouter = require("/routes");
+app.use("/", indexRouter);
+
+app.get("*", (req, res) => {
+  res.render("404");
+});
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
