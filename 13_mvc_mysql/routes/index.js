b8fd7ce06e59c695a81ac79b8c7961d0e1b81670
Uichae <ghddmlco@gmail.com>
2023-04-06 23:05:31 +0900

    오류 해결

--- 13_mvc_mysql/routes/index.js
+++ 13_mvc_mysql/routes/index.js
@@ -2,11 +2,22 @@ const express = require("express");
 const controller = require("../controller/Cvisitor");
 const router = express.Router();
 
+// GET / => localhost:PORT/
 router.get("/", controller.main);
 
-router.get("/visitor", controller.getVisitors); // 전체 조희
+// GET /visitor => localhost:PORT/visitor
+router.get("/visitor", controller.getVisitors); // 전체 조회
 
-//POST /visitor/write
-router.post("/visitor/write", controller.postVisitor); // 요청이 들어오면 하나 추가
+// POST /visitor/write
+router.post("/visitor/write", controller.postVisitor); // 하나 추가
+
+// GET /visitor/get
+router.get("/visitor/get", controller.getVisitor); // 하나 조회
+
+// PATCH /visitor/edit
+router.patch("/visitor/edit", controller.patchVisitor); // 하나 수정
+
+// DELETE /visitor/delete
+router.delete("/visitor/delete", controller.deleteVisitor); // 하나 삭제
 
 module.exports = router;
