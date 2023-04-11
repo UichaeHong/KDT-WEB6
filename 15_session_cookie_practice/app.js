0f5a86a5aa6fc55895e8f3f0065185f212cb347b
Uichae <ghddmlco@gmail.com>
2023-04-11 17:13:46 +0900

    cookie 실습문제  풀이

--- 15_session_cookie_practice/app.js
+++ 15_session_cookie_practice/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8000;
 
@@ -8,61 +9,32 @@ app.use("/static", express.static(__dirname + "/static"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// // cookie-parser 모듈 불러오기
-// // : 요청의 쿠키를 해석해서 req.cookies 객체로 만듦
-// // ex. name=hello 라는 쿠키를 보내면, req.cookies => { name: 'hello' }
-// const cookieParser = require("cookie-parser");
-
-// // cookieParser(secretKey, optionObj);
-// // - secretKey: 비밀키
-// // - optionObj: 쿠키에서 사용할 옵션 값
-// app.use(cookieParser()); // req.cookies 가능 해짐
-
-// // res.cookie(key, value, options)
-// // : 쿠키 "설정"
-
-// // res.clearCookie(key, value, options)
-// // : 쿠키 "삭제"
-
-// const cookieConfing = {
-//   httpOnly: true, // 웹 서버를 통해서만 쿠키 접근 가능
-//   maxAge: 60 * 1000, // 쿠키 수명 (ms 단위)
-//   // expires: // 만료 날짜 설정
-//   // secure: https 에서만 쿠키 접근
-//   // signed: 쿠키 암호화
-// };
+app.use(cookieParser());
+// 쿠키 옵션 설정
+const cookieConfig = {
+  httpOnly: true,
+  maxAge: 1000 * 60 * 60 * 24, // 24h
+};
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { popup: req.cookies.popup }); // cookie에 접근, popup이라는 변수값으로 cookie를 찾음
 });
 
-// // 1. 쿠키 설정
-// app.get("/setCookie", (req, res) => {
-//   // 쿠키 설정
-//   res.cookie("my first cookie", "cookie value test", cookieConfing);
-
-//   // 클라이언트 응답 보냄
-//   res.send("쿠키 설정 완료");
-// });
-
-// // 2. 쿠키 확인
-// app.get("/getCookie", (req, res) => {
-//   // req.cookies
-//   // : cookie-parser 모듈이 만든 요청의 쿠키를 읽어옴
-//   res.send(req.cookies);
-// });
-
-// // 3. 쿠키 삭제
-// app.get("/clearCookie", (req, res) => {
-//   // 쿠키 삭제
-//   // clearCookie 할 때, res.cookie() 설정했던 key & value & option 일치해야 함
-//   // (단, option에서 expires, maxAge 옵션은 일치하지 않아도 괜찮!)
-//   res.clearCookie("my first cookie", "cookie value test", cookieConfing);
-
-//   // 클라이언트 응답
-//   res.send("쿠키 삭제 완료!!");
-// });
+app.post("/setCookie", (req, res) => {
+  // 쿠키 생성
+  res.cookie("popup", "hide", cookieConfig);
+  res.send("쿠키 설정 완료");
+});
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
+
+// cookie 정리!
+
+// req 객체
+// req.cookies: cookie-parser가 만드는 요청의 쿠키를 해석한 객체
+
+// res 객체
+// res.cookie(key, value, options): 쿠키 설정 메서드
+// res.clearCookie(key, value, options): 쿠키 삭제 메서드
