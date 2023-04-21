53607345614c2413f5e99e1f58a13c96f2358c7f
Uichae <ghddmlco@gmail.com>
2023-04-21 16:03:43 +0900

    실습문제

--- 22_react_04_eventhandling/src/Classcomponent.js
+++ 22_react_04_eventhandling/src/Classcomponent.js
@@ -11,9 +11,9 @@ class ClassComponent extends React.Component {
     // this.printConsole = this.printConsole.bind(this);
   }
 
-  printConsole() {
+  printConsole = () => {
     console.log("버튼 클릭 >>", this.state);
-  }
+  };
 
   printConsole2 = (msg) => {
     console.log(msg);
@@ -24,11 +24,49 @@ class ClassComponent extends React.Component {
         클래스형 컴포넌트에서 이벤트 사용해보기
         {/* classcomponent에서 함수를 이용할 때 앞에 this를 입력해야 함 */}
         <button onClick={this.printConsole}>print console</button>
-        <button onClick={() => this.printConsole2("hihi")}>
-          print console
-        </button>
+        <button onClick={() => this.printConsole2("hihi")}>print msg</button>
       </div>
     );
   }
 }
 export default ClassComponent;
+
+// import React from "react";
+
+// class ClassComponent extends React.Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       name: "Codingon",
+//     };
+
+//     // this.printConsole = this.printConsole.bind(this);
+//   }
+
+//   // printConsole() {
+//   //   console.log(this);
+//   //   console.log('버튼 클릭! >> ', this.state);
+//   // }
+
+//   printConsole = () => {
+//     // console.log(this);
+//     console.log("버튼 클릭! >> ", this.state);
+//   };
+
+//   printConsole2 = (msg) => {
+//     console.log(msg);
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         클래스형 컴포넌트에서 이벤트 사용해보기
+//         <button onClick={this.printConsole}>print console</button>
+//         <button onClick={() => this.printConsole2("hihi")}>print msg</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default ClassComponent;
