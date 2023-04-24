9ee39cb46d9a71100d093e60670e2ec34b9eb176
Uichae <ghddmlco@gmail.com>
2023-04-25 00:45:29 +0900

    블로그 작성

--- 17_react_distruct/index4.js
+++ 17_react_distruct/index4.js
@@ -88,23 +88,23 @@
 // reduce(callback)
 // 배열의 각 요소에 주어진 리듀스 함수를 실행하고 하나의 결과값을 반환
 
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
-// 1 + 2 + 3 + 4 + 5
-const sum = numbers.reduce((acc, currentValue) => {
-  return acc + currentValue;
-});
-console.log(sum);
-//  풀이
-//    l
-//    v
+// // 1 + 2 + 3 + 4 + 5
+// const sum = numbers.reduce((acc, currentValue) => {
+//   return acc + currentValue;
+// });
+// console.log(sum);
+// //  풀이
+// //    l
+// //    v
 
-// acc (누적합)
-const sum2 = numbers.reduce((acc, currentValue) => {
-  console.log(`${idx} 번째 인덱스`);
-  console.log(`acc : ${acc}`);
-  console.log(`currentValue : ${currentValue}`);
+// // acc (누적합)
+// const sum2 = numbers.reduce((acc, currentValue) => {
+//   console.log(`${idx} 번째 인덱스`);
+//   console.log(`acc : ${acc}`);
+//   console.log(`currentValue : ${currentValue}`);
 
-  return acc + currentValue;
-});
-console.log(sum2);
+//   return acc + currentValue;
+// });
+// console.log(sum2);
